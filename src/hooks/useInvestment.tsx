bbeef340b8d62fdb725bcfb511c384644dd25c16
Fabricio Miranda <fabriciomirandabr@gmail.com>
2022-06-26 06:41:18 -0400

    Force Refetch

--- src/hooks/useInvestment.tsx
+++ src/hooks/useInvestment.tsx
@@ -1,13 +1,23 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useReactiveVar } from '@apollo/client'
+import { useEffect } from 'react'
 import { InvestmentData, InvestmentVars, INVESTMENT_QUERY } from '../graphql/query/Investment'
+import { loadingVar } from '../variables/TransactionVariable'
 
 export const useInvestment = (collectionAddress: string, chainId: number) => {
+  const loadingTX = useReactiveVar(loadingVar)
+
   const { loading, data, error, refetch } = useQuery<InvestmentData, InvestmentVars>(INVESTMENT_QUERY, {
     variables: {
       collectionAddress,
       chainId: chainId
-    }
+    },
+    pollInterval: 3000,
+    fetchPolicy: 'network-only'
   })
+
+  useEffect(() => {
+    refetch()
+  }, [loadingTX])
 
   return {
     loading,
